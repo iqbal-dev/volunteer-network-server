1cc9fc325d83499d371717feb71e37e77466b37e
iqbal-dev <pkmdiqbal@gmail.com>
2020-10-17 15:48:23 +0600

    updated

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const ObjectId = require('mongodb').ObjectId
+const ObjectId = require('mongodb').ObjectId;
+const fileUpload = require('express-fileupload');
+const fs = require('fs-extra');
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j1hzz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -14,6 +16,8 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('doctors'))
+app.use(fileUpload())
 
 
 
@@ -26,10 +30,33 @@ client.connect(err => {
       
         console.log(volunteer)
     })
-    app.post('/addEvent', (req, res) => {
-        volunteerCollection.insertOne(req.body)
-        console.log(req.body)
-    })
+    app.post('/newService', (req, res) => {
+        const title = req.body.title;
+        const description = req.body.description;
+        const date = req.body.date;
+        const file = req.files.files;
+    
+        const filePath = `${__dirname}/doctors/${file.name}`;
+        file.mv(filePath, err => {
+            if (err) {
+                console.log(err);
+            }
+            const newImg = req.files.files.data;
+            const encImg = newImg.toString('base64');
+    
+            var image = {
+                content: file.mimetype,
+                size: file.size,
+                img: Buffer.from(encImg, 'base64')
+    
+            }
+            volunteerCollection.insertOne({ title, description,date, image })
+                .then(result => {
+                    res.send(result)
+                })
+    
+        })
+    });
     app.get('/getVolunteer', (req, res) => {
         volunteerCollection.find({})
             .toArray((err, documents) => {
@@ -62,13 +89,14 @@ client.connect(err => {
     app.delete('/delete/:id', (req, res) => {
         volunteerDetailCollection.deleteOne({ _id: ObjectId(req.params.id) })
             .then(result => {
-            
+            console.log(result)
         })
     })
     app.delete('/cancel/:id', (req, res) => {
         volunteerDetailCollection.deleteOne({ _id: ObjectId(req.params.id) })
-            .then(res => {
-            
+            .then(result => {
+                res.send(result);
+                console.log(result)
         })
     })
 
